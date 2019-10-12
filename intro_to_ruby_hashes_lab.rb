041253b50bbb7e65de9498f7e5c5f6c168a98789
yash patel <yash82099@yahoo.com>
2019-10-12 02:36:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,11 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+	
+	monopoly = {
+	  :rainroads=> {}
+	}
+	return monopoly
 end
 
 def monopoly_with_second_tier
@@ -9,6 +13,15 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monopoly_with_second_tier = {
+	  :rainroads=> {
+	    :pieces=>4,
+	    :rent_in_dollars=>{},
+	    :names=>{}
+	  }
+	}
+  
+
 end
 
 def monopoly_with_third_tier
@@ -16,6 +29,39 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monopoly_with_third_tier_tier = {
+    :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
+    :names=>
+    {
+      :reading_railroad=>
+      {
+        
+      },
+      :pennsylvania_railroad=>
+      {
+        
+      },
+      :b_and_o_railroad=>
+      {
+       
+      },
+      :shortline=>
+      {
+        
+      }
+    }
+  }
+  }
+  
 end
 
 def monopoly_with_fourth_tier
@@ -23,4 +69,36 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monopoly_with_fourth_tier = {
+    :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
+    :names=>
+    {
+      :reading_railroad=>
+      {
+        
+      },
+      :pennsylvania_railroad=>
+      {
+        
+      },
+      :b_and_o_railroad=>
+      {
+       
+      },
+      :shortline=>
+      {
+        
+      }
+    }
+  }
+  }
 end
