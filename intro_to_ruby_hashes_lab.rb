f58a673d0b23451bf38afdc4c0525ebf02097134
yash patel <yash82099@yahoo.com>
2019-10-12 02:56:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   monopoly_with_second_tier = {
-	  :railroads => { :pieces=> "4"}
+	  :railroads => { :pieces=> 4}
 	}
 	return monopoly_with_second_tier
   
