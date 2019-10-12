5c9771199e3deae08027447fa47ed4c004f74a54
yash patel <yash82099@yahoo.com>
2019-10-12 02:59:13 +0000

    Done.

--- final_hash.rb
+++ final_hash.rb
@@ -5,15 +5,16 @@ monopoly =  {
   :railroads=>
   {
     :pieces=>4,
-    :rent_in_dollars=>
-    {
-      :one_piece_owned=>25,
+    :rent_in_dollars=> 
+    { 
+      :one_piece_owned=>25, 
       :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
+      :three_pieces_owned=>100, 
       :four_pieces_owned=>200
+      
     },
     :names=>
-    {
+    { 
       :reading_railroad=>
       {
         "mortgage_value"=>"$100"
