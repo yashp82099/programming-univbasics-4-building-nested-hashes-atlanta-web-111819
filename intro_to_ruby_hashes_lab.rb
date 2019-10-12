80aef5334180140fa6b91cbb22730cc32cb671a7
yash patel <yash82099@yahoo.com>
2019-10-12 02:58:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -81,19 +81,19 @@ def monopoly_with_fourth_tier
     {
       :reading_railroad=>
       {
-        
+        "mortgage_value"=>"$100"
       },
       :pennsylvania_railroad=>
       {
-        
+        "mortgage_value"=>"$200"
       },
       :b_and_o_railroad=>
       {
-       
+        "mortgage_value"=>"$400"
       },
       :shortline=>
       {
-        
+        "mortgage_value"=>"$800"
       }
     }
   }
