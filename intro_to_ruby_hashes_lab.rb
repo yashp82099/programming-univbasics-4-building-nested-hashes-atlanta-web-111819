7e01dee5d15e4824a79480bd8172c7c1579ed817
yash patel <yash82099@yahoo.com>
2019-10-12 02:46:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,9 +15,7 @@ def monopoly_with_second_tier
   # complexity.
   monopoly_with_second_tier = {
 	  :rainroads=> {
-	    pieces: '4',
-	    rent_in_dollars: {},
-	    names: {}
+	    pieces: {}
 	  }
 	}
 	return monopoly_with_second_tier
